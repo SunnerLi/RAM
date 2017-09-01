7a526f1b4f4f7f3c27fa70975e6380424646cec8
SunnerLi <a6214123@gmail.com>
2017-09-01 10:42:42 +0800

    use tf.Variable() to fix the reuse error

--- layer.py
+++ layer.py
@@ -1,34 +1,24 @@
-from tensorlayer.layers import Layer
 from config import *
 import tensorlayer as tl
 import tensorflow as tf
 
-class GlimpsExtractLayer(Layer):
-  def __init__(
-      self,
-      layer = None,
-      loc_tensor = None,
-      act = tf.nn.relu,
-      name ='simple_dense',
-  ):
-      # check layer name (fixed)
-      Layer.__init__(self, name=name)
+weight = []
+bias = []
 
-      # the input of this layer is the output of previous layer (fixed)
-      self.inputs = layer.outputs
+class Dense(object):
+    def __new__(self, input_tensor, n_units=2, activation_fn=tf.identity, name=None):
+        global weight
+        global bias
+        if name == None:
+            print('error! You should assign name of variable scope')
+            exit()
+        with tf.variable_scope(name) as vs:
+            W = tf.Variable(tf.random_normal((int(input_tensor.get_shape()[1]), n_units), mean=0.0), dtype=tf.float32)
+            b = tf.Variable(tf.random_normal((n_units,), dtype=tf.float32))            
+            weight.append(W)
+            bias.append(b)
+            return tf.add(tf.matmul(input_tensor, W), b)
 
-      # operation (customized)
-      n_in = int(self.inputs._shape[-1])
-      self.loc_tensor = loc_tensor
-      with tf.variable_scope(name) as vs:
-
-          # tensor operation
-          self.outputs = tf.image.extract_glimpse(self.inputs, [win_size, win_size], self.loc_tensor)
-
-      # get stuff from previous layer (fixed)
-      self.all_layers = list(layer.all_layers)
-      self.all_params = list(layer.all_params)
-      self.all_drop = dict(layer.all_drop)
-
-      # update layer (customized)
-      self.all_layers.extend( [self.outputs] )
+if __name__ == '__main__':
+    _input = tf.placeholder(tf.float32, [None, 28 * 28 * 1])
+    layer = Dense(_input, n_units=16)
--- network.py
+++ network.py
@@ -8,52 +8,29 @@ class GlimpsNetwork(object):
         self.location_ph = location_ph
 
     def getGlimps(self, loc_tensor):
-        """
-        self.input_layer = tl.layers.InputLayer(self.location_ph, name='glimps_input_layer')
-        self.glimps_imgs = tl.layers.ReshapeLayer(self.input_layer, [-1, 28, 28, 1], name='reshape_layer_1')
-        self.glimps_imgs = GlimpsExtractLayer(self.glimps_imgs, location_ph)
-        self.glimps_imgs = tl.layers.ReshapeLayer(self.glimps_imgs, [-1, win_size * win_size * 1], name='reshape_layer_2')
-        """
         self.glimps_imgs = tf.reshape(self.location_ph, [-1, 28, 28, 1], name='reshape_layer_1')
         self.glimps_imgs = tf.image.extract_glimpse(self.glimps_imgs, [win_size, win_size], loc_tensor)
         self.glimps_imgs = tf.reshape(self.glimps_imgs, [-1, win_size * win_size * 1])
         return self.glimps_imgs
 
     def __call__(self, loc_tensor):
-        """
         self.retina_imgs = self.getGlimps(loc_tensor)
-        self.retina_net = tl.layers.DenseLayer(self.retina_imgs, n_units = 128, act = tf.nn.relu, name='glimps_ind_fc_1')
-        self.location_input_layer = tl.layers.InputLayer(loc_tensor, name='location_state_input')
-        self.location_net = tl.layers.DenseLayer(self.location_input_layer, n_units = 128, act = tf.nn.relu, name='glimps_ind_fc_2')
-        self.glimps_net = tl.layers.ConcatLayer([self.retina_net, self.location_net], concat_dim = -1)
-        self.glimps_net = tl.layers.DenseLayer(self.glimps_net, n_units = 256, act = tf.nn.relu, name='glimps_merge_fc_1')
-        return self.glimps_net.outputs
-        """
-        self.retina_imgs = self.getGlimps(loc_tensor)
-        self.retina_imgs = layers.fully_connected(inputs=self.retina_imgs, num_outputs=128, activation_fn=tf.nn.relu)
-        self.location_net = layers.fully_connected(inputs=loc_tensor, num_outputs=128, activation_fn=tf.nn.relu)
+        self.retina_imgs = tf.nn.relu(Dense(self.retina_imgs, n_units=128, name='glimps_ind_fc_1'))
+        self.location_net = tf.nn.relu(Dense(loc_tensor, n_units=128, name='glimps_ind_fc_2'))
         self.glimps_net = tf.concat([self.retina_imgs, self.location_net], axis=-1)
-        self.glimps_net = layers.fully_connected(inputs=self.glimps_net, num_outputs=256, activation_fn=tf.identity)
+        self.glimps_net = tf.nn.relu(Dense(self.glimps_net, n_units=256, name='glimps_merge_fc_1'))
         return self.glimps_net
 
 class LocationNetwork(object):
-    def __init__(self):
-        pass
-
     def __call__(self, state_tensor):
-        """
-        self.location_net = tl.layers.InputLayer(state_tensor)
-        print (tf.get_variable_scope().reuse)
-        self.location_net = tl.layers.DenseLayer(self.location_net, n_units = loc_dim, name='location_net_fc1')
-        """
         with tf.variable_scope(tf.get_variable_scope()) as vs:
-            self.location_net = layers.fully_connected(inputs=state_tensor, num_outputs=loc_dim, activation_fn=tf.identity, scope=vs, reuse=True)
+            self.location_net = Dense(state_tensor, n_units=loc_dim, name='location_net_fc1')
             self.mean = tf.stop_gradient(tf.clip_by_value(self.location_net, -1.0, 1.0))
             self.location = self.mean + tf.random_normal((tf.shape(state_tensor)[0], loc_dim), stddev=loc_std)
             self.location = tf.stop_gradient(self.location)
             return self.location, self.mean
 
-
-location_ph = tf.placeholder(tf.float32, [None, 28, 28, 1])
-net = LocationNetwork()
-net(tf.random_uniform((1, 2), minval=-1, maxval=1))
+if __name__ == '__main__':
+    location_ph = tf.placeholder(tf.float32, [None, 28, 28, 1])
+    net = LocationNetwork()
+    net(tf.random_uniform((1, 2), minval=-1, maxval=1))
